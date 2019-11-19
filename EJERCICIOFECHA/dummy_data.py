be98ae184a6e3d052074a9b41a693bc42f3481e7
gonzapython <56170708+gonzapython@users.noreply.github.com>
2019-11-19 19:25:27 +0100

    por fin!  subo el main con jsonify

--- EJERCICIOFECHA/dummy_data.py
+++
@@ -1,5 +0,0 @@
-dia = 12
-mes = 11
-anio = 2019
-hora = 14
-minutos = 44
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def enviar_datos():
     if request.method == 'POST':
         data = request.json
         update_from_front(data)
-        return data
-
+        result = update_from_front(data)
+        return jsonify(result)
 
 if __name__ == "__main__":
     app.run()
